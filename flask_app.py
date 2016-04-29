7044a6820264de7a4380a08b1ab018999ec55288
Sumbhav <cheeto10x@yahoo.com>
2016-04-29 16:20:38 -0700

    Added AngularJS into the mix. Learning time!

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,44 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import jsonify
+from flask import render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return app.send_static_file('/index.html')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/games/<string:date>')
+def games_for_date(date):
+    #Get the result from nba.com
+    url = 'http://stats.nba.com/stats/scoreboard?DayOffset=0&LeagueID=00&GameDate=' + date
+    #BORROWED FROM seemsthere's nba_py
+    headers = {'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+                              'AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/45.0.2454.101 Safari/537.36'),
+               'referer': 'http://stats.nba.com/scores/'
+            }
+    r = requests.get(url, headers=headers)
+    try:
+        result = r.json()
+    except:
+        return jsonify(result='Error')
+
+    #Get the relevant rows
+    row_set = result['resultSets'][0]['rowSet']
+    games = dict()
+    for row in row_set:
+        game_id = row[2]
+        name = str(row[5]).split('/')[1]
+        name_formatted = name[:3] + '@' + name[3:]
+        games[name_formatted] = game_id
+
+
+    return jsonify(result='Success', gameList=games)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
